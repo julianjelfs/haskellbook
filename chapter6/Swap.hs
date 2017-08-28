33ad6a68ccab60b0cbb9b072d4e94355a95c0d1f
jjelfs <jjelfs@travelrepublic.co.uk>
2017-08-28 12:24:35 +0100

    more chpt 6 stuff

---
+++ chapter6/Swap.hs
@@ -0,0 +1,23 @@
+module Swap where
+
+import Data.List
+
+jung :: [Int] -> Int
+jung xs = head $ sort xs
+
+young :: Ord a => [a] -> a
+young = head . sort
+
+mySort :: Ord a => [a] -> [a]
+mySort = sort
+
+signifier :: Ord a => [a] -> a
+signifier xs = head (mySort xs)
+
+chk :: Eq b => (a -> b) -> a -> b -> Bool
+chk aTob a b = b == aTob a
+
+arith :: Num b => (a -> b) -> Integer -> a -> b
+arith aTob i a = b * (fromInteger i)
+    where
+        b = aTob a
