1a517af5c640d2da4c40ff6815797eaa025751cb
jjelfs <jjelfs@travelrepublic.co.uk>
2017-08-29 22:31:19 +0100

    chpt7 exercises

---
+++ chapter7/Anon.hs
@@ -0,0 +1,13 @@
+module Anon where
+
+addOneIfOdd n = case odd n of
+    True -> f n
+    False -> n
+    where f  = \n -> n + 1
+
+addFive = \x y -> (if x > y then y else x) + 5
+
+mFlip f x y = f x y
+
+f :: (a,b,c) -> (d,e,f) -> ((a,d), (c,f))
+f (a,b,c) (d,e,f) = ((a,d), (c,f))
---
+++ chapter7/Case.hs
@@ -0,0 +1,24 @@
+module Case where
+
+functionC x y =
+    case x > y of
+        True -> x
+        False -> y
+
+ifEvenAdd2 n =
+    case even n of
+        True -> n + 2
+        False-> n
+
+nums x =
+    case compare x 0 of
+        LT -> -1
+        GT -> 1
+        EQ -> 0
+
+dodgy x y = x + y * 10
+
+oneIsOne = dodgy 1
+
+oneIsTwo = (flip dodgy) 2
+
