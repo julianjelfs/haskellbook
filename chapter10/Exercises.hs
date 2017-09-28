fe7562f58da27b54e3752d08349cbfb3b8f32cff
jjelfs <jjelfs@travelrepublic.co.uk>
2017-09-28 17:31:15 +0100

    started chapter10 exercises

---
+++ chapter10/Exercises.hs
@@ -0,0 +1,18 @@
+module Exercises where
+
+stops = "pbtdkg"
+
+vowels = "aeiou"
+
+nouns = ["tv", "hat", "dog", "table", "telescope"]
+
+verbs = ["hits", "likes", "butchers", "dives", "mutates"]
+
+combos l1 l2 =
+  [ (s, v, s) | s <- l1, v <- l2 ]
+
+letterCombos =
+  filter (\(s, _, _) -> s == 'p') $ combos stops vowels
+
+wordCombos =
+  combos nouns verbs
