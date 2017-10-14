3b18f27722aede6405a894a1a30a05ab7437af43
jjelfs <jjelfs@travelrepublic.co.uk>
2017-10-14 10:11:19 +0100

    finished cpht 11

---
+++ chapter11/HuttonsRazor.hs
@@ -0,0 +1,16 @@
+module HuttonsRazor where
+
+data Expr
+  = Lit Integer
+  | Add Expr Expr
+
+
+testExpr = (Add (Add (Lit 10) (Lit 20) ) (Add (Lit 50) (Lit 1000)))
+
+eval :: Expr -> Integer
+eval (Lit n) = n
+eval (Add l r) = eval l + eval r
+
+printExpr :: Expr -> String
+printExpr (Lit n) = show n
+printExpr (Add l r) = printExpr l ++ " + " ++ printExpr r
