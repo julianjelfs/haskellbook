05d815ed1d900a526ebbc8fe2401f59ec112dbf6
jjelfs <jjelfs@travelrepublic.co.uk>
2017-10-22 19:06:38 +0100

    unfolding tree

--- chapter12/Unfolds.hs
+++ chapter12/Unfolds.hs
@@ -2,12 +2,33 @@ module Unfolds where
 
 import           Data.List
 
+data BinaryTree a
+  = Leaf
+  | Node (BinaryTree a)
+         a
+         (BinaryTree a)
+  deriving (Eq, Ord, Show)
+
+unfoldTree :: (a -> Maybe (a, b, a)) -> a -> BinaryTree b
+unfoldTree f a =
+  case f a of
+    Nothing        -> Leaf
+    Just (x, y, z) -> Node (unfoldTree f x) y (unfoldTree f z)
+
+treeBuild :: Integer -> BinaryTree Integer
+treeBuild n =
+  unfoldTree
+    (\a ->
+       if a == n
+         then Nothing
+         else Just (a + 1, a, a + 1))
+    0
+
 myIterate :: (a -> a) -> a -> [a]
 myIterate f x = [x] ++ myIterate f (f x)
 
 betterIterate :: (a -> a) -> a -> [a]
-betterIterate f x =
-  myUnfoldr (\b -> Just (b, f b)) x
+betterIterate f x = myUnfoldr (\b -> Just (b, f b)) x
 
 myUnfoldr :: (b -> Maybe (a, b)) -> b -> [a]
 myUnfoldr f x =
