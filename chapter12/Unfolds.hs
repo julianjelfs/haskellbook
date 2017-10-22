e12b3849121dee3461630eb163e43c104ff66a20
jjelfs <jjelfs@travelrepublic.co.uk>
2017-10-22 11:45:24 +0100

    more unfolds, hurts my head

--- chapter12/Unfolds.hs
+++ chapter12/Unfolds.hs
@@ -1,6 +1,27 @@
 module Unfolds where
 
-myIterate :: (a -> a) -> a -> [a]
-myIterate f x =
-  [x] ++ myIterate f (f x)
+import           Data.List
 
+myIterate :: (a -> a) -> a -> [a]
+myIterate f x = [x] ++ myIterate f (f x)
+
+betterIterate :: (a -> a) -> a -> [a]
+betterIterate f x =
+  myUnfoldr (\b -> Just (b, f b)) x
+
+myUnfoldr :: (b -> Maybe (a, b)) -> b -> [a]
+myUnfoldr f x =
+  case f x of
+    Just (a, b) -> [a] ++ myUnfoldr f b
+    Nothing     -> []
+
+testUnfold = take 5 $ myUnfoldr (\b -> Just (b, b + 1)) 0
+
+testUnfold2 =
+  take 20 $
+  myUnfoldr
+    (\b ->
+       if b > 5
+         then Nothing
+         else (Just (b, b + 1)))
+    0
