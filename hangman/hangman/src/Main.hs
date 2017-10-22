830ca4a8d102375762af1e4cebda416e7419b897
jjelfs <jjelfs@travelrepublic.co.uk>
2017-10-22 20:43:57 +0100

    tired now

--- hangman/hangman/src/Main.hs
+++ hangman/hangman/src/Main.hs
@@ -1,5 +1,12 @@
 module Main where
 
+import           Control.Monad (forever)
+import           Data.Char     (toLower)
+import           Data.List     (intersperse)
+import           Data.Maybe    (isJust)
+import           System.Exit   (exitSuccess)
+import           System.Random (randomRIO)
+
 main :: IO ()
 main = do
   putStrLn "hello world"
