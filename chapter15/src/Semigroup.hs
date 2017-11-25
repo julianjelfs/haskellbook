bd373b92ae551c8fd00a01d1bc3f0e59ebe7541b
jjelfs <jjelfs@travelrepublic.co.uk>
2017-11-25 12:01:55 +0000

    more semigroups

--- chapter15/src/Semigroup.hs
+++ chapter15/src/Semigroup.hs
@@ -7,6 +7,12 @@ newtype Biggest a =
   Biggest a
   deriving (Eq, Show)
 
+instance (Ord a) => Semigroup (Biggest a) where
+  (<>) (Biggest a) (Biggest b)
+    | a > b = Biggest a
+    | a < b = Biggest b
+    | otherwise = Biggest a
+
 newtype Shortest a =
   Shortest [a]
   deriving (Eq, Show)
@@ -17,11 +23,24 @@ instance Semigroup (Shortest a) where
     | length a < length b = Shortest a
     | otherwise = Shortest a
 
-instance (Ord a) => Semigroup (Biggest a) where
-  (<>) (Biggest a) (Biggest b)
-    | a > b = Biggest a
-    | a < b = Biggest b
-    | otherwise = Biggest a
+data Two a b = Two a b deriving (Show, Eq)
+
+instance (Semigroup a, Semigroup b) => Semigroup (Two a b) where
+  (Two a1 b1) <> (Two a2 b2) = Two (a1 <> a2) (b1 <> b2)
+
+twoGen :: (Arbitrary a, Arbitrary b) => Gen (Two a b)
+twoGen = do
+  a <- arbitrary
+  b <- arbitrary
+  return (Two a b)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+  arbitrary = twoGen
+
+data Three a b c = Three a b c
+
+data Four a b c d = Four a b c d
+
 
 shortestGen :: Arbitrary a => Gen (Shortest a)
 shortestGen = do
@@ -39,17 +58,25 @@ biggestGen = do
 instance Arbitrary a => Arbitrary (Biggest a) where
   arbitrary = biggestGen
 
+
 semigroupAssociativity :: (Eq s, Semigroup s) => s -> s -> s -> Bool
 semigroupAssociativity x y z =
   x <> (y <> z) == (x <> y) <> z
 
 type S = Shortest Integer
 type B = Biggest Integer
+type T = Two String String
+type ShortestAssoc = S -> S -> S -> Bool
+type BiggestAssoc = B -> B -> B -> Bool
+type TwoAssoc = T -> T -> T-> Bool
 
 testShortestAssoc :: IO ()
 testShortestAssoc = do
-  quickCheck (semigroupAssociativity :: S -> S -> S -> Bool)
+  quickCheck (semigroupAssociativity :: ShortestAssoc)
 
-testBiggestAssoc :: IO ()
-testBiggestAssoc = do
-  quickCheck (semigroupAssociativity :: B -> B -> B -> Bool)
+testAssoc :: IO ()
+testAssoc = do
+  quickCheck (semigroupAssociativity :: ShortestAssoc)
+  quickCheck (semigroupAssociativity :: BiggestAssoc)
+  quickCheck (semigroupAssociativity :: TwoAssoc)
+
