967f7c0e02bfaee8330de24defd7947aa42efd77
jjelfs <jjelfs@travelrepublic.co.uk>
2017-12-06 21:55:30 +0000

    functor exercises

--- chapter16/src/Exercises.hs
+++ chapter16/src/Exercises.hs
@@ -11,6 +11,9 @@ functorCompose :: (Eq (f c), Functor f) =>
 functorCompose x (Fun _ f) (Fun _ g) =
   (fmap (g . f) x) == (fmap g . fmap f $ x)
 
+------------------------------------------------------
+--  Identity
+------------------------------------------------------
 newtype Identity a = Identity a deriving (Eq, Show)
 
 instance Functor Identity where
@@ -30,3 +33,33 @@ type IntToInt = Fun Int Int
 testIdentity = do
   quickCheck (functorCompose :: Identity String -> StrToStr -> StrToStr -> Bool)
   quickCheck (functorCompose :: Identity Int -> IntToInt -> IntToInt -> Bool)
+
+
+------------------------------------------------------
+--  Pair
+------------------------------------------------------
+--data Pair a = Pair a a deriving (Eq, Show)
+
+--instance Functor Pair where
+--  fmap f (Pair x y) = Pair x y
+
+------------------------------------------------------
+--  Two
+------------------------------------------------------
+data Two a b = Two a b deriving (Eq, Show)
+
+instance Functor (Two a) where
+  fmap f (Two a b) = Two a (f b)
+
+twoGen :: (Arbitrary a, Arbitrary b) => Gen (Two a b)
+twoGen = do
+  a <- arbitrary
+  b <- arbitrary
+  return $ Two a b
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+  arbitrary = twoGen
+
+testTwo = do
+  quickCheck (functorCompose :: Two String String -> StrToStr -> StrToStr -> Bool)
+  quickCheck (functorCompose :: Two Int Int -> IntToInt -> IntToInt -> Bool)
