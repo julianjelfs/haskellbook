087afb495afbbd3dc73a875f629d415f37920615
jjelfs <jjelfs@travelrepublic.co.uk>
2017-12-28 20:09:34 +0000

    chapt 17 ex

--- chapter17/Exercises.hs
+++ chapter17/Exercises.hs
@@ -29,3 +29,24 @@ maxed = max' <$> a <*> b
 summed :: Maybe Integer
 summed = undefined  -- not sure what to do with this one
 
+newtype Identity a = Identity a
+  deriving (Eq, Ord, Show)
+
+instance Functor Identity where
+  fmap f (Identity a) = Identity (f a)
+
+instance Applicative Identity where
+  pure a = Identity a
+  (Identity f) <*> (Identity a) = Identity (f a)
+
+newtype Constant a b =
+  Constant { getConstant :: a }
+  deriving (Eq, Ord, Show)
+
+instance Functor (Constant a) where
+  fmap f (Constant a) = Constant a
+
+instance Monoid a => Applicative (Constant a) where
+  pure b = Constant mempty
+  (Constant f) <*> (Constant b) = Constant b
+
