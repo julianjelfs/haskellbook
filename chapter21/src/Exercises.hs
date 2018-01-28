683e45079ee304e1905dc5951f4b34994ae2a62d
jjelfs <jjelfs@travelrepublic.co.uk>
2018-01-28 17:50:29 +0000

    finished traversable - not sure I really get it though

--- chapter21/src/Exercises.hs
+++ chapter21/src/Exercises.hs
@@ -62,4 +62,90 @@ instance Traversable Optional where
   traverse f Nada = pure Nada
   traverse f (Yep x) = Yep <$> f x
 
+--List
+data List a
+  = Nil
+  | Cons a (List a)
+  deriving Show
 
+append x Nil = Cons x Nil
+append x (Cons y Nil) = Cons y (Cons x Nil)
+append x (Cons y l) = (Cons y (append x l))
+
+instance Monoid (List a) where
+  mempty = Nil
+  Nil `mappend` l = l
+  l `mappend` Nil = l
+  l1 `mappend` (Cons x l2) = append x l1 `mappend` l2
+
+instance Functor List where
+  fmap _ Nil = Nil
+  fmap f (Cons x l) = Cons (f x) (fmap f l)
+
+instance Foldable List where
+  foldMap _ Nil = mempty
+  foldMap f (Cons x l) = f x `mappend` foldMap f l
+
+--don't understand that at all
+instance Traversable List where
+  traverse f Nil = pure Nil
+  traverse f (Cons x l) = Cons <$> f x <*> traverse f l
+
+--three
+data Three a b c = Three a b c deriving Show
+
+instance Functor (Three a b) where
+  fmap f (Three x y z) = Three x y (f z)
+
+instance Foldable (Three a b) where
+  foldMap f (Three x y z) = f z
+
+instance Traversable (Three a b) where
+  traverse f (Three x y z) = Three x y <$> f z
+
+--three'
+data Three' a b = Three' a b b deriving Show
+
+instance Functor (Three' a) where
+  fmap f (Three' x y z) = Three' x (f y) (f z)
+
+instance Foldable (Three' a) where
+  foldMap f (Three' x y z) = f y `mappend` f z
+
+instance Traversable (Three' a) where
+  traverse f (Three' x y z) = Three' x <$> f y <*> f z
+
+--S
+data S n a = S (n a) a deriving Show
+
+instance Functor n => Functor (S n) where
+  fmap f (S n a) = S (fmap f n) (f a)
+
+instance Foldable n => Foldable (S n) where
+  foldMap f (S n a) = foldMap f n `mappend` f a
+
+instance Traversable n => Traversable (S n) where
+  traverse f (S n a) = S <$> traverse f n <*> f a
+
+--Tree
+data Tree a
+  = Empty
+  | Leaf a
+  | Node (Tree a) a (Tree a)
+  deriving (Eq, Show)
+
+instance Functor Tree where
+  fmap _ Empty = Empty
+  fmap f (Leaf a) = Leaf (f a)
+  fmap f (Node l n r) = Node (fmap f l) (f n) (fmap f r)
+
+instance Foldable Tree where
+  foldMap _ Empty = mempty
+  foldMap f (Leaf a) = f a
+  foldMap f (Node l n r) = foldMap f l `mappend` (f n) `mappend` foldMap f r
+
+instance Traversable Tree where
+  traverse _ Empty = pure Empty
+  traverse f (Leaf a) = Leaf <$> f a
+  traverse f (Node l n r) =
+    Node <$> traverse f l <*> f n <*> traverse f r
