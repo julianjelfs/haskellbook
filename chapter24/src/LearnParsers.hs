5e5cf30272a638be5773856e938e8cb2e1b1258d
jjelfs <jjelfs@travelrepublic.co.uk>
2018-02-18 17:15:20 +0000

    parsers

--- chapter24/src/LearnParsers.hs
+++ chapter24/src/LearnParsers.hs
@@ -6,13 +6,20 @@ import Text.Parser.Combinators
 stop :: Parser a
 stop = unexpected "stop"
 
+oneS :: CharParsing m => m String
+oneS = string "1"
+
 one = char '1' >> eof
 
 one' = one >> stop
 
-oneTwo = char '1' >> char '2'
+oneTwo = char '1' >> char '2' >> eof
 
 oneTwo' = oneTwo >> stop
+
+testParseS :: Parser String -> IO()
+testParseS p =
+    print $ parseString p mempty "123"
 
 testParse :: Parser Char -> IO()
 testParse p =
@@ -33,7 +40,7 @@ main = do
     pNL "one':"
     testParse one'
     pNL "oneTwo:"
-    testParse oneTwo
+    testEOF oneTwo
     pNL "oneTwo':"
-    testParse oneTwo'
+    testEOF oneTwo'
     
