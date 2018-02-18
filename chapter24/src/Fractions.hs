53f72fbcc71679b68174e55a3e668f539a55a184
jjelfs <jjelfs@travelrepublic.co.uk>
2018-02-18 17:43:13 +0000

    rational parser

---
+++ chapter24/src/Fractions.hs
@@ -0,0 +1,17 @@
+module Fractions where
+
+import Control.Applicative
+import Data.Ratio ((%))
+import Text.Trifecta
+
+badFraction = "1/0"
+alsoBad = "10"
+shouldWork = "1/2"
+shouldAlsoWork = "2/1"
+
+parseFraction :: Parser Rational
+parseFraction = do
+  numerator <- decimal
+  char '/'
+  denominator <- decimal
+  return (numerator % denominator)
