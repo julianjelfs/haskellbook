7ef048448465a2d0c539f91f4f94d59030d7501d
jjelfs <jjelfs@travelrepublic.co.uk>
2018-03-03 10:40:21 +0000

    phone parser

--- chapter24/src/PhoneParser.hs
+++ chapter24/src/PhoneParser.hs
@@ -14,5 +14,34 @@ data PhoneNumber =
   PhoneNumber NumberingPlanArea Exchange LineNumber
   deriving (Eq, Show)
 
+intParser :: Parser Int
+intParser = do
+  d <- some digit
+  return $ read d
+
+digits :: Int -> Parser Int
+digits n = read <$> count n digit
+
 parsePhone :: Parser PhoneNumber
-parsePhone = undefined
+parsePhone =
+  PhoneNumber <$> npaParser <*> exchangeParser <*> lineParser
+
+npaParser :: Parser NumberingPlanArea
+npaParser =
+  choice
+    [ try $ digits 1 *> char '-' *> digits 3 <* (optional (char '-'))
+    , digits 3 <* (optional (char '-'))
+    , char '(' *> digits 3 <* string ") "
+    ]
+
+exchangeParser :: Parser Exchange
+exchangeParser = digits 3 <* (optional (char '-'))
+
+lineParser :: Parser LineNumber
+lineParser = digits 4
+
+main = do
+  print $ doParse parsePhone "123-456-7890"
+  print $ doParse parsePhone "1234567890"
+  print $ doParse parsePhone "(123) 456-7890"
+  print $ doParse parsePhone "1-123-456-7890"
