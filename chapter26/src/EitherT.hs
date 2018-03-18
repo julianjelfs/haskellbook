b1469762e3b5dd445334f02feb94c1771ec34aaf
jjelfs <jjelfs@travelrepublic.co.uk>
2018-03-18 21:17:25 +0000

    EitherT functor instance

--- chapter26/src/EitherT.hs
+++ chapter26/src/EitherT.hs
@@ -4,7 +4,7 @@ newtype EitherT e m a =
     EitherT { runEitherT :: m (Either e a) }
 
 instance Functor m => Functor (EitherT e m) where
-  fmap = undefined
+  fmap f (EitherT me) = EitherT $ (fmap . fmap) f me
 
 instance Applicative m => Applicative (EitherT e m) where
   pure = undefined
@@ -15,6 +15,14 @@ instance Monad m => Monad (EitherT e m) where
   return = pure
 
   v >>= f = undefined
+
+
+swapEitherT :: (Functor m) => EitherT e m a -> EitherT a m e
+swapEitherT = undefined
+
+swapEither :: Either e a -> Either a e
+swapEither (Left e) = Right e
+swapEither (Right a) = Left a
 
     
 
