0b790d3543c7f3304363b43bdbe38e83f598590a
jjelfs <jjelfs@travelrepublic.co.uk>
2018-03-18 21:44:40 +0000

    EitherT applicative & monad instances

--- chapter26/src/EitherT.hs
+++ chapter26/src/EitherT.hs
@@ -7,18 +7,21 @@ instance Functor m => Functor (EitherT e m) where
   fmap f (EitherT me) = EitherT $ (fmap . fmap) f me
 
 instance Applicative m => Applicative (EitherT e m) where
-  pure = undefined
-
-  f <*> a = undefined
+  pure x = EitherT (pure (pure x))
+  (EitherT fe) <*> (EitherT me) = EitherT $ (<*>) <$> fe <*> me
 
 instance Monad m => Monad (EitherT e m) where
   return = pure
-
-  v >>= f = undefined
+  (EitherT me) >>= f =
+    EitherT $ do
+      v <- me
+      case v of
+        Left err -> return (Left err)
+        Right ok -> runEitherT (f ok)
 
 
 swapEitherT :: (Functor m) => EitherT e m a -> EitherT a m e
-swapEitherT = undefined
+swapEitherT (EitherT me) = EitherT $ fmap swapEither me
 
 swapEither :: Either e a -> Either a e
 swapEither (Left e) = Right e
