4f408ef28b92bc2511f6ecef729def612b611628
Julian Jelfs <julian.jelfs@gmail.com>
2018-05-07 22:40:35 +0100

    started filling in sqllite boilerplate

--- fingerd/src/Main.hs
+++ fingerd/src/Main.hs
@@ -1,5 +1,66 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module Main where
 
+import Control.Exception
+import Control.Monad (forever)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+import Data.List (intersperse)
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Text.Encoding (decodeUtf8, encodeUtf8)
+import Data.Typeable
+import Database.SQLite.Simple hiding (close)
+import qualified Database.SQLite.Simple as SQLite
+import Database.SQLite.Simple.Types
+import Network.Socket hiding (close, recv)
+import Network.Socket.ByteString (recv, sendAll)
+import Text.RawString.QQ
+
+data User = User
+  { userId :: Integer
+  , username :: Text
+  , shell :: Text
+  , homeDirectory :: Text
+  , realName :: Text
+  , phone :: Text
+  } deriving (Eq, Show)
+
+instance FromRow User where
+  fromRow = User <$> field <*> field <*> field <*> field <*> field <*> field
+
+instance ToRow User where
+  toRow (User id_ username shell homeDir realName phone) =
+    toRow (id_, username, shell, homeDir, realName, phone)
+
+createUser :: Query
+createUser =
+  " CREATE TABLE IF NOT EXISTS users \
+  \  (id INTEGER PRIMARY KEY AUTOINCREMENT, \
+  \  username TEXT UNIQUE, \
+  \  shell TEXT, \
+  \  homeDirectory TEXT, \
+  \  realName TEXT, \
+  \  phone TEXT)"
+
+insertUser :: Query
+insertUser = "INSERT INTO users values (?, ?, ?, ?, ?, ?)"
+
+allUsers :: Query
+allUsers = "SELECT * from users"
+
+getUserQuery :: Query
+getUserQuery = "SELECT * from users where username = ?"
+
+data DuplicateData =
+  DuplicateData
+  deriving (Eq, Show, Typeable)
+
+instance Exception DuplicateData
+
+type UserRow = (Null, Text, Text, Text, Text, Text)
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = putStrLn "hello world"
